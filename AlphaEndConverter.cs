0877cdd9f9320d4caad24aff360b407e00e33acf
Oaz <github@azeau.com>
2011-05-15 20:54:33 +0200

    we removed some duplication

--- AlphaEndConverter.cs
+++ AlphaEndConverter.cs
@@ -21,10 +21,7 @@ namespace CanUDoIt
 		public string Convert(int number)
 		{
       char[] digits = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'x', 'y', 'z' };
-      if(number >= 13)
-			  return digits[number/13].ToString() + digits[number%13].ToString();
-      else
-			  return digits[number%13].ToString();
+			return ((number >= 13) ? digits[number/13].ToString():"") + digits[number%13].ToString();
 		}
 	}
 
