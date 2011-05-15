9f0420ffb1674d42f4b482ca95169cd231286ae9
Oaz <github@azeau.com>
2011-05-15 20:56:16 +0200

    a bit of recursion

--- AlphaEndConverter.cs
+++ AlphaEndConverter.cs
@@ -21,7 +21,7 @@ namespace CanUDoIt
 		public string Convert(int number)
 		{
       char[] digits = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'x', 'y', 'z' };
-			return ((number >= 13) ? digits[number/13].ToString():"") + digits[number%13].ToString();
+			return ((number >= 13) ? Convert(number/13):"") + digits[number%13].ToString();
 		}
 	}
 
@@ -35,7 +35,7 @@ namespace CanUDoIt
 			Assert.AreEqual("x", new Converter().Convert(10));
 			Assert.AreEqual("10", new Converter().Convert(13));
 			Assert.AreEqual("17", new Converter().Convert(20));
-			//Assert.AreEqual("2381", new Converter().Convert(5006));
+			Assert.AreEqual("2381", new Converter().Convert(5006));
 			//Assert.AreEqual("20z1879", new Converter().Convert(9999999));
 		}
 
