29ef12bcc9e140c75f3961ce8fc26cd47ece53eb
Oaz <github@azeau.com>
2011-05-15 20:53:15 +0200

    straighforward implementation - needs refactoring

--- AlphaEndConverter.cs
+++ AlphaEndConverter.cs
@@ -21,7 +21,10 @@ namespace CanUDoIt
 		public string Convert(int number)
 		{
       char[] digits = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'x', 'y', 'z' };
-			return digits[number/13].ToString() + digits[number%13].ToString();
+      if(number >= 13)
+			  return digits[number/13].ToString() + digits[number%13].ToString();
+      else
+			  return digits[number%13].ToString();
 		}
 	}
 
